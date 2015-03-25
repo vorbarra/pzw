7fe6254486c566c135305074fdb88e82db7c607a
vorbarra <bmijat00@fesb.hr>
2015-03-25 16:26:59 +0100

    napravljena zadaca1

--- Domaci/MainWindow.xaml.cs
+++ Domaci/MainWindow.xaml.cs
@@ -24,5 +24,42 @@ namespace Domaci
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.LeftButton.Click += LeftButton_Click;
+            this.RightButton.Click += RightButton_Click;
+        }
+
+        void RightButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rectangle = new Rectangle()
+                {
+                  Width=400,
+                  Height=50,
+                  Margin = new Thickness(10),
+                  Fill = Brushes.Red
+                };
+             
+
+            this.RightContainer.Children.Add(rectangle);
+        }
+
+        void LeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rectangle = new Rectangle()
+            {
+                Width = 120,
+                Height = 100,
+                Margin = new Thickness(5),
+                Fill = Brushes.Orange
+            };
+
+
+            this.LeftContainer.Children.Add(rectangle);
+        }
+
+        
+
     }
 }
